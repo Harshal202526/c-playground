288d8cc30ec07d6a7f2561584dd42dc57849a696
Harshal202526 <harshalgawai644@gmail.com>
2025-08-10 12:16:20 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    
+    cout<<"\ninput a";
+    cin>>a;
+    cout<<"\ninput b";
+    cin>>b;
+    
+    c=a+b;
+    cout<<"\nyour addition is "<<c;\
+    
+    return 0;
+}
